7fa4d8087d127dfe10c3ea12a5c3b78a8587007b
saisumanth007 <sumanth4591@gmail.com>
2018-03-21 18:36:39 +0530

    Add conv1d case1

--- src/model.py
+++ src/model.py
@@ -27,3 +27,28 @@ def RandomForest():
 
 	regr = RandomForestRegressor(max_depth=6,random_state=0)
 	return regr
+
+def Conv1d_test_1(in_dim, out_dim):
+
+	inp = Input(shape = (in_dim,1))
+	hour = Input(shape = (1,))
+	month = Input(shape = (1,))
+	week_day = Input(shape = (1,))
+	month_day = Input(shape = (1,))
+	weekend_flag = Input(shape = (1,))
+
+	conv_1 = Conv1D(4,10,activation='relu')(inp)
+	pool1 = MaxPooling1D(2)(conv_1)
+	flatten = Flatten()(pool1)
+
+	conc = Concatenate(1)([flatten, hour, month, week_day, month_day, weekend_flag])
+	dense1 = Dense(20, activation = 'relu')(conc)
+	dense2 = Dense(20, activation = 'relu')(dense1)
+	out = Dense(out_dim)(dense2)
+
+	input_layers = [inp, hour, month, week_day, month_day, weekend_flag]
+	model = Model(inputs = input_layers,outputs = out)
+	model.compile(optimizer="adam",loss="mae")
+
+	return model
+
