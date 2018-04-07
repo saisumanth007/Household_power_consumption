4255b254e37fbeffdb4379f3a52d35a2f6e153d3
saisumanth007 <sumanth4591@gmail.com>
2018-04-07 20:34:09 +0530

    Try residuals

--- src/model.py
+++ src/model.py
@@ -87,9 +87,9 @@ def squeeze_arch(in_dim, out_dim):
 	y = BatchNormalization()(y)
 	y = Activation('relu')(y)
 	y = squeeze_excite_block(y)
-	y_2 = add([y, y_1])
+	# y_2 = add([y, y_1])
 
-	y = Conv1D(128, 3, padding='same', kernel_initializer='he_uniform')(y_2)
+	y = Conv1D(128, 3, padding='same', kernel_initializer='he_uniform')(y)
 	y = BatchNormalization()(y)
 	y = Activation('relu')(y)
 	y = add([y, y_2])
